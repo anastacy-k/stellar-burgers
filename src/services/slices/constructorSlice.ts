edfc71b135204bcf6be01c43da293e4f6fac7bf1
Анастасия <minigulova10@gmail.com>
2026-06-09 19:32:55 +0300

    перенесен метод uuidv4() из редьюсера в prepare

--- src/services/slices/constructorSlice.ts
+++ src/services/slices/constructorSlice.ts
@@ -48,17 +48,17 @@ export const constructorSlice = createSlice({
   name: 'burgerConstructor',
   initialState,
   reducers: {
-    addIngredient: (state, action: PayloadAction<TIngredient>) => {
-      const ingredient: TConstructorIngredient = {
-        ...action.payload,
-        id: uuidv4()
-      };
-
-      if (action.payload.type === 'bun') {
-        state.bun = ingredient;
-      } else {
-        state.ingredients.push(ingredient);
-      }
+    addIngredient: {
+      reducer: (state, { payload }: PayloadAction<TConstructorIngredient>) => {
+        if (payload.type === 'bun') {
+          state.bun = payload;
+        } else {
+          state.ingredients.push(payload);
+        }
+      },
+      prepare: (ingredient: TIngredient) => ({
+        payload: { ...ingredient, id: uuidv4() }
+      })
     },
     removeIngredient: (state, action: PayloadAction<string>) => {
       state.ingredients = state.ingredients.filter(
